750493327bd8042634b2fcedda01935565331ef2
Hallux <guru@connected.rocks>
2017-04-06 22:31:24 +0200

    prod

--- prod/main.js
+++ prod/main.js
@@ -16,6 +16,7 @@ module.exports.loop = function () {
     Game.spawns['Spawn1'].createCreep([CARRY, MOVE, WORK], 'upgrader8', {role: 'upgrader'});
     Game.spawns['Spawn1'].createCreep([CARRY, MOVE, WORK], 'upgrader9', {role: 'upgrader'});
     //Game.spawns['Spawn1'].createCreep([CARRY, MOVE, WORK], 'upgrader9', {role: 'upgrader'});
+    //Game.spawns['Spawn1'].createCreep([CARRY, MOVE, WORK], 'upgrader9', {role: 'upgrader'});
     
     
     var tower = 0; Game.getObjectById('2a428d5f087b8fb11656db33');
